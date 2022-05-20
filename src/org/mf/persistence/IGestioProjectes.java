35b3f7dedcf01b970a38315c92ebdf0ad2ba0545
mespejo@milaifontanals.org <marionaespejo@gmail.com>
2022-05-20 15:05:22 +0200

    Actualitzacions pujada

--- src/org/mf/persistence/IGestioProjectes.java
+++ src/org/mf/persistence/IGestioProjectes.java
@@ -1,16 +1,12 @@
 package org.mf.persistence;
 
 import java.util.List;
-import org.mf.classes.Estat;
-import org.mf.classes.Projecte;
-import org.mf.classes.Tasca;
-import org.mf.classes.Usuari;
 
 public interface IGestioProjectes {
 
     List<Object> Login(String user, String password) throws GestioProjectesException;
 
-    List<Projecte> GetProjectes(String session_id) throws GestioProjectesException;
+   /* List<Projecte> GetProjectes(String session_id) throws GestioProjectesException;
 
     List<Tasca> GetTasquesAssignades(String session_id) throws GestioProjectesException;
 
@@ -26,7 +22,7 @@ public interface IGestioProjectes {
 
     int NovaEntrada(String session_id, String novaEntrada, Estat estat) throws GestioProjectesException;
 
-    int NovaEntrada(String session_id, String novaEntrada) throws GestioProjectesException;
+    int NovaEntrada(String session_id, String novaEntrada) throws GestioProjectesException;*/
 
 	/**
      * Tanca la connexió amb el SGBD
